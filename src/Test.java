718b6008537f48267c6e506e532bfc47bcca758a
stakagi <Shinichi.Takagi@ha.ax>
2020-03-28 21:24:17 +0200

    Add all my files

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+import java.util.Optional;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String str = null;
+//		System.out.println(str.length());
+		
+		Optional<String> strOpt = Optional.ofNullable(str);
+		
+		if(strOpt.isPresent()) {
+			String message = strOpt.get();
+			System.out.println(message.length());
+		} else {
+			System.out.println("The string is null");
+		}
+		
+		strOpt.ifPresent(v -> System.out.println(v.length()));
+	}
+
+}
